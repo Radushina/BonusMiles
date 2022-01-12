c360fc8cbc911b033dfcea1083a944a76415d729
Radushina <nataradush@gmail.com>
2022-01-12 15:49:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int cost = 2051; // цена билета
+        int coeff = 20; // количество рублей для одной мили
+        int miles = cost / coeff; // количество миль
+        System.out.println(miles);
+    }
+}
+
